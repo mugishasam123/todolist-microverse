0079f7dc73e0fe633c865284e3a4d7ddff853d86
mugisha Samuel <mugishasamuel42@gmail.com>
2022-02-16 05:41:52 -0800

    Revert "Add all todo list features"

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import TodoContainer from './components/TodoContainer';
-import reportWebVitals from './reportWebVitals';
-
-ReactDOM.render(<TodoContainer />, document.getElementById('root'));
-
-reportWebVitals();
